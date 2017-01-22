013bac8bb106d0101e8d41766f66912359ab08cc
Matti Ahinko <matti.ahinko@gofore.com>
2017-01-22 12:26:04 +0200

    Add checking item

--- stock_updater.py
+++ stock_updater.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from multiple_products_found_error import MultipleProductsFoundError
+from product_not_found_error import ProductNotFoundError
+
 class StockUpdater:
 
     def __init__(self, db_connection):
@@ -21,10 +24,30 @@ class StockUpdater:
         for item in self.items:
             self.update_quantity(item['product_code'], item['quantity'])
 
+    def check_product(self, product_code):
+        cursor = self.db_connection.cursor()
+
+        check_query = "SELECT COUNT(*) FROM {} WHERE {} LIKE ?".format(
+            self.table, self.product_code_column)
+
+        item_count = cursor.execute(check_query, (product_code,)).fetchone()[0]
+        cursor.close()
+
+        if item_count == 0:
+            raise ProductNotFoundError(
+                "No product found with product code {}"
+                .format(product_code))
+        elif item_count > 1:
+            raise MultipleProductsFoundError(
+                "Multiple products found with product code {}"
+                .format(product_code))
+
     def update_quantity(self, product_code, quantity):
+        self.check_product(product_code)
+
+        cursor = self.db_connection.cursor()
         query = "UPDATE {} SET {} = ? WHERE {} LIKE ?".format(
             self.table, self.quantity_column, self.product_code_column)
-        cursor = self.db_connection.cursor()
         try:
             cursor.execute(query, (quantity, product_code))
             self.db_connection.commit()
