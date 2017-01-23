4714c13768e6dda23dc82bcbaf402b33fe521f18
Matti Ahinko <matti.ahinko@gofore.com>
2017-01-23 19:45:10 +0200

    Divide tests

--- tests/test_stock_updater.py
+++ tests/test_stock_updater.py
@@ -31,13 +31,33 @@ class TestStockUpdater(unittest.TestCase):
         conn.commit()
         cursor.close()
 
+    def create_updater(self, conn):
+        updater = StockUpdater(conn)
+        updater.set_destination_colums("product_code", "quantity")
+        updater.set_table("products")
+        return updater
+
+    def test_update_single(self):
+        conn = sqlite3.connect(DATABASE_FILE)
+        self.create_database(conn)
+
+        updater = self.create_updater(conn)
+
+        updater.update_quantity("123", 15)
+
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT quantity FROM products WHERE product_code LIKE '123'")
+        row = cursor.fetchone()
+        self.assertEqual(15, row[0])
+        cursor.close()
+        conn.close()
+
     def test_update_all(self):
         conn = sqlite3.connect(DATABASE_FILE)
         self.create_database(conn)
 
-        updater = StockUpdater(conn)
-        updater.set_destination_colums("product_code", "quantity")
-        updater.set_table("products")
+        updater = self.create_updater(conn)
         updater.set_items([
             { "product_code": "123", "quantity": 10 },
             { "product_code": "456", "quantity": 15 }
@@ -56,6 +76,22 @@ class TestStockUpdater(unittest.TestCase):
         cursor.close()
         conn.close()
 
+    def test_update_multiple_hits_single(self):
+        conn = sqlite3.connect(DATABASE_FILE)
+        self.create_database(conn)
+
+        cursor = conn.cursor()
+        cursor.execute(
+            ("INSERT INTO products (product_code, quantity)"
+            "VALUES ('456', 0)"))
+        conn.commit()
+        cursor.close()
+
+        updater = self.create_updater(conn)
+
+        with self.assertRaises(MultipleProductsFoundError):
+            updater.update_quantity("456", 15)
+
     def test_update_multiple_hits(self):
         conn = sqlite3.connect(DATABASE_FILE)
         self.create_database(conn)
@@ -67,12 +103,7 @@ class TestStockUpdater(unittest.TestCase):
         conn.commit()
         cursor.close()
 
-        updater = StockUpdater(conn)
-        updater.set_destination_colums("product_code", "quantity")
-        updater.set_table("products")
-
-        with self.assertRaises(MultipleProductsFoundError):
-            updater.update_quantity("456", 15)
+        updater = self.create_updater(conn)
 
         updater.set_items([
             { "product_code": "123", "quantity": 10 },
@@ -81,15 +112,25 @@ class TestStockUpdater(unittest.TestCase):
         with self.assertRaises(MultipleProductsFoundError):
             updater.update()
 
+    def test_not_found_single(self):
+        conn = sqlite3.connect(DATABASE_FILE)
+        self.create_database(conn)
+
+        updater = self.create_updater(conn)
+
+        with self.assertRaises(ProductNotFoundError):
+            updater.update_quantity("1234", 15)
+
+
     def test_not_found(self):
         conn = sqlite3.connect(DATABASE_FILE)
         self.create_database(conn)
 
-        updater = StockUpdater(conn)
-        updater.set_destination_colums("product_code", "quantity")
-        updater.set_table("products")
+        updater = self.create_updater(conn)
+
         updater.set_items([
-            { "product_code": "1234", "quantity": 10 }
+            { "product_code": "1234", "quantity": 10 },
+            { "product_code": "456", "quantity": 15 }
         ])
         with self.assertRaises(ProductNotFoundError):
             updater.update()
