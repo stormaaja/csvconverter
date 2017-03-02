8c5a77ed452715bad667eddcb3f633ec89f2c20d
Matti Ahinko <matti.ahinko@gofore.com>
2017-03-02 21:20:07 +0200

    Create log dir

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import os
 
 from update_wrapper import UpdateWrapper
 
+if not os.path.isdir("log"):
+    os.mkdir("log")
+
 wrapper = UpdateWrapper()
 wrapper.read_config("config.json")
 wrapper.run()
