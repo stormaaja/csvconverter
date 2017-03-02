252da0e680881e41f4f9cf10fa427ab829ac972e
Matti Ahinko <matti.ahinko@gofore.com>
2017-03-02 21:19:54 +0200

    Move update logic to own class

--- main.py
+++ main.py
@@ -1,58 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from urllib2 import Request, urlopen, HTTPError, URLError
-from csv_converter import CsvConverter
-from stock_updater import StockUpdater
-import sqlite3
-import mysql.connector
 import os
-import json
 
-import database_helper
+from update_wrapper import UpdateWrapper
 
-with open("config.json") as f:
-    config = json.load(f)
-
-request = Request(config["source"]["url"])
-
-try:
-    response = urlopen(request)
-except HTTPError as e:
-    print "The server returned error {}".format(e.code)
-    exit
-except URLError as e:
-    print "Failed to reach server: {}".format(e.reason)
-    exit
-
-converter = CsvConverter("")
-if "debug" in config:
-    converter.set_debug(config["debug"])
-converter.setSourceColumns(
-    config["source"]["product_code_column"],
-    config["source"]["quantity_column"])
-converter.read_csv(response)
-
-if config["database_connection"]["type"] == "sqlite3":
-    conn = sqlite3.connect(config["database_connection"]["database"])
-elif config["database_connection"]["type"] == "mysql":
-    conn = mysql.connector.connect(
-        user=config["database_connection"]["username"],
-        password=config["database_connection"]["password"],
-        host=config["database_connection"]["host"],
-        database=config["database_connection"]["database"])
-else:
-    raise "Please, define database"
-
-if config["testing"]:
-    database_helper.initialize(conn)
-    database_helper.add_test_products(conn)
-
-updater = StockUpdater(conn)
-updater.set_perform_check_product(config["database_connection"]["check_products"])
-updater.set_destination_colums(
-    config["database_connection"]["product_code_column"],
-    config["database_connection"]["quantity_column"])
-updater.set_table(config["database_connection"]["products_table"])
-updater.set_items(converter.rows)
-updater.update()
+wrapper = UpdateWrapper()
+wrapper.read_config("config.json")
+wrapper.run()
---
+++ update_wrapper.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from urllib2 import Request, urlopen, HTTPError, URLError
+from csv_converter import CsvConverter
+from stock_updater import StockUpdater, ProductNotFoundError, MultipleProductsFoundError
+import sqlite3
+import mysql.connector
+import os
+import json
+import logging
+from datetime import datetime
+
+import database_helper
+
+logging.captureWarnings(True)
+
+class UpdateWrapper:
+
+    def __init__(self):
+        logging.basicConfig(
+            filename="log/{}.log".format(datetime.now().strftime("%Y%m%d%H%M%S%%f")),
+            level=logging.DEBUG)
+
+    def read_config(self, file):
+        with open(file) as f:
+            self.config = json.load(f)
+
+    def run(self):
+        request = Request(self.config["source"]["url"])
+
+        try:
+            response = urlopen(request)
+        except HTTPError as e:
+            logging.error("The server returned error %d", e.code)
+            raise e
+        except URLError as e:
+            logging.error("Failed to reach server: %s", e.reason)
+            raise e
+
+        converter = CsvConverter("")
+
+        converter.setSourceColumns(
+            self.config["source"]["product_code_column"],
+            self.config["source"]["quantity_column"])
+        converter.read_csv(response)
+
+        if self.config["database_connection"]["type"] == "sqlite3":
+            conn = sqlite3.connect(self.config["database_connection"]["database"])
+        elif self.config["database_connection"]["type"] == "mysql":
+            conn = mysql.connector.connect(
+                user=self.config["database_connection"]["username"],
+                password=self.config["database_connection"]["password"],
+                host=self.config["database_connection"]["host"],
+                database=self.config["database_connection"]["database"])
+        else:
+            raise "Please, define database"
+
+        if self.config["testing"]:
+            database_helper.initialize(conn)
+            database_helper.add_test_products(conn)
+
+        updater = StockUpdater(conn)
+        updater.set_destination_colums(
+            self.config["database_connection"]["product_code_column"],
+            self.config["database_connection"]["quantity_column"])
+        updater.set_table(self.config["database_connection"]["products_table"])
+
+        for item in converter.rows:
+            try:
+                updater.update_quantity(item['product_code'], item['quantity'])
+            except ProductNotFoundError as e:
+                logging.warning("Product {} not found".format(item['product_code']))
+            except MultipleProductsFoundError as e:
+                logging.error("Multiple products found with product id {}".format(item['product_code']))
