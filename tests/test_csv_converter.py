b4d9d96212eb193338a7ca70d76614719dcec0c9
Matti Ahinko <matti.ahinko@gofore.com>
2017-03-02 20:35:20 +0200

    Tweak tests

--- tests/test_csv_converter.py
+++ tests/test_csv_converter.py
@@ -4,6 +4,8 @@ sys.path.insert(0, os.path.dirname(__file__))
 sys.path.insert(1, os.path.dirname(".."))
 
 import unittest
+import warnings
+
 from csv_converter import CsvConverter
 
 class TestCsvConverter(unittest.TestCase):
@@ -13,7 +15,19 @@ class TestCsvConverter(unittest.TestCase):
         converter.setSourceColumns("tuotekoodi", "qty")
         converter.read_file()
 
-    def test_convert_row(self):
+    def test_parse_invalid_csv(self):
+        converter = CsvConverter("tests/data/data_2.csv")
+        converter.setSourceColumns("tuotekoodi", "qty")
+
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("always")
+            converter.read_file()
+
+            assert len(w) == 3
+            assert issubclass(w[-1].category, UserWarning)
+            assert "Invalid row" in str(w[-1].message)
+
+    def test_convert_row_success(self):
         converter = CsvConverter("")
         row = converter.convertRow({
             "product_code": "some_code",
@@ -21,18 +35,25 @@ class TestCsvConverter(unittest.TestCase):
         })
         self.assertEqual("some_code", row["product_code"])
 
+    def test_convert_row_invalid_quantity(self):
+        converter = CsvConverter("")
+
         with self.assertRaises(ValueError):
             row = converter.convertRow({
                 "product_code": "23",
                 "quantity": "error"
             })
 
+    def test_convert_row_invalid_product_code(self):
+        converter = CsvConverter("")
         with self.assertRaises(ValueError):
             row = converter.convertRow({
                 "product_code": "",
                 "quantity": "error"
             })
 
+    def test_convert_row_invalid_values(self):
+        converter = CsvConverter("")
         with self.assertRaises(ValueError):
             row = converter.convertRow({
                 "product_code": "sd",
